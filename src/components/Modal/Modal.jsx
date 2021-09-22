ba49ff705b698988c29751b4a45cc387f247bc2e
Artur <shevchenkoartur.745@gmail.com>
2021-09-22 17:55:41 +0300

    refactored modal markup

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -7,15 +7,17 @@ const Modal = ({closeModal, user}) => {
             <div className={style.content} onClick={(e) => e.stopPropagation()}>
                 <div className={style.body}>
                     <span onClick={() => closeModal()} className={style.close}>&times;</span>
-                    <img src={user.picture} alt="img"/>
-                    <p>Name: {user.name}</p>
-                    <p>Age: {user.age}</p>
-                    <p>Gender: {user.gender}</p>
-                    <p>Eye Color: {user.eyeColor}</p>
-                    <p>Company: {user.company}</p>
-                    <p>Email: {user.email}</p>
-                    <p>Phone: {user.phone}</p>
-                    <p>Address: {user.address}</p>
+                    <div>
+                        <img src={user.picture} alt="img"/>
+                        <p>Name: {user.name}</p>
+                        <p>Age: {user.age}</p>
+                        <p>Gender: {user.gender}</p>
+                        <p>Eye Color: {user.eyeColor}</p>
+                        <p>Company: {user.company}</p>
+                        <p>Email: {user.email}</p>
+                        <p>Phone: {user.phone}</p>
+                        <p>Address: {user.address}</p>
+                    </div>
                 </div>
             </div>
         </div>
